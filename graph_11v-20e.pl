5336a1c5754bea9aeb9be9847936d295a5f80319
linduxed <linduxed@gmail.com>
2012-11-07 15:24:05 +0100

    Added sample graph.

---
+++ graph_11v-20e.pl
@@ -0,0 +1,21 @@
+% link(From, To, Distance).
+link(p1  , p2  , 1).
+link(p2  , p4  , 4).
+link(p2  , p5  , 1).
+link(p3  , p1  , 2).
+link(p3  , p10 , 2).
+link(p4  , p1  , 3).
+link(p4  , p5  , 2).
+link(p5  , p6  , 5).
+link(p5  , p9  , 1).
+link(p6  , p11 , 1).
+link(p7  , p6  , 4).
+link(p7  , p10 , 3).
+link(p8  , p5  , 3).
+link(p8  , p7  , 1).
+link(p8  , p9  , 5).
+link(p9  , p3  , 4).
+link(p9  , p4  , 1).
+link(p10 , p8  , 1).
+link(p11 , p6  , 2).
+link(p11 , p7  , 1).

6dd733a03d471c0d9e3d87a15f90b9e67fa1f690
linduxed <linduxed@gmail.com>
2012-11-09 00:54:56 +0100

    Length constricted path finding implemented.
    Only problem now is that if one asks for "next solution" on the last
    possible solution, an infinite loop occurs.

--- pathfinder.pl
+++ pathfinder.pl
@@ -1,8 +1,26 @@
+%
 % Consult a graph file before executing.
+%
 
+% Wrapping predicates for finding shortest path.
+fsp(Start, End, Path, TotalLength) :-
+	lowestLengthFP(Start, End, 1, Path, TotalLength).
+
+lowestLengthFP(Start, End, RequiredLength, Path, TotalLength) :-
+	fp(Start, End, Path, RecursiveLength),
+	RequiredLength =:= RecursiveLength,
+	TotalLength is RecursiveLength.
+
+lowestLengthFP(Start, End, RequiredLength, Path, TotalLength) :-
+	lowestLengthFP(Start, End, RequiredLength+1, Path, TotalLength).
+
+
+% Wrapping predicate for finding path.
 fp(Start, End, Path, TotalLength) :-
 	findPath(Start, End, [Start], Path, TotalLength).
 
+
+% Actual pathfinding.
 %%%% findPath(+Start, +End, +PathSoFar, -Path, -TotalLength)
 findPath(Previous, End, PathSoFar, Path, TotalLength) :-
 	link(Previous, End, TotalLength),
